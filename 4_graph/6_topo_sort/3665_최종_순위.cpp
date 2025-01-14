cd36431185684d2c4502ec85a5b261d0fe757a1b
chickenchobab <hew980812@gmail.com>
2025-01-14 23:09:09 +0900

    topological sort

---
+++ 4_graph/6_topo_sort/3665_최종_순위.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+int n, m;
+int t[501];
+int ranking[501];
+// vector<int> graph[501];
+int inDegree[501];
+
+// void topoSort(){
+//   queue<int> q;
+//   vector<int> sequence;
+
+//   for (int i = 1; i <= n; ++i){
+//     if (inDegree[i]) continue;
+//     q.push(i);
+//   }
+
+//   while (!q.empty()){
+//     if (q.size() > 1){
+//       cout << "?";
+//       return;
+//     }
+
+//     int cur = q.front();
+//     sequence.push_back(cur);
+//     q.pop();
+
+//     for (int nxt : graph[cur]){
+//       if (--inDegree[nxt] == 0)
+//         q.push(nxt);
+//     }
+//   }
+
+//   if (sequence.size() != n){
+//     cout << "IMPOSSIBLE";
+//     return;
+//   }
+//   for (int num : sequence)
+//     cout << num << ' ';
+// }
+
+// bool inOrder(int a, int b){
+//   return find(begin(graph[b]), end(graph[b]), a) == end(graph[b]);
+// }
+
+// void completeGraph(){
+//   for (int i = 1; i <= n; ++i)
+//     for (int j = 1; j < ranking[i]; ++j)
+//       if (inOrder(t[j], i))
+//         graph[t[j]].push_back(i);
+// }
+
+void solve(){
+
+  // completeGraph();
+  // topoSort();
+
+  // 모든 상대적 정보가 주어졌다면 ?는 불가능하며
+  // indegree가 01234임을 이용함
+  vector<int> visited(n + 1, 0);
+  bool flag = 1;
+
+  for (int i = 1; i <= n; ++i){
+    if (!visited[inDegree[i]])
+      visited[inDegree[i]] = i;
+    else {
+      flag = 0;
+      break;
+    }
+  }
+
+  if (flag)
+    for (int i = 0; i < n; ++i)
+      cout << visited[i] << ' ';
+  else 
+    cout << "IMPOSSIBLE";
+  cout << '\n';
+}
+
+void init(){
+  cin >> n;
+  for (int r = 1; r <= n; ++r){
+    cin >> t[r];
+    ranking[t[r]] = r;
+    inDegree[t[r]] = r - 1;
+  }
+  // for (int i = 1; i <= n; ++i){
+  //   graph[i].clear();
+  //   inDegree[i] = 0;
+  // }
+  cin >> m;
+  int a, b;
+  while (m--){
+    cin >> a >> b;
+    if (ranking[a] < ranking[b]){
+      // graph[b].push_back(a);
+      --inDegree[b];
+      ++inDegree[a];
+    }
+    else {
+      // graph[a].push_back(b);
+      --inDegree[a];
+      ++inDegree[b];
+    }
+  }
+}
+
+int main(){
+  fastio
+  int T;
+  cin >> T;
+  while (T--){
+    init();
+    solve();
+  }
+  return 0;
+}
--- 5_dp/2_general/2482.cpp
+++ 5_dp/2_general/2482.cpp
@@ -7,26 +7,34 @@ using namespace std;
 using ll = long long;
 
 int N, K;
-int dp[2][1000][1000];
+int dp[1000][1000];
 
 void solve(){
+  int a, b;  
+    
   // 0번을 고른 경우
   for (int start = 0; start < N - 1; ++start)
-    dp[1][0][start] = 1;
+    dp[0][start] = 1;
 
   for (int i = 1; i < K; ++i)
     for (int j = 2 * i; j < N - 1; ++j)
-      dp[1][i][j] = (dp[1][i - 1][j - 2] + dp[1][i][j - 1]) % DIVISOR;
+      dp[i][j] = (dp[i - 1][j - 2] + dp[i][j - 1]) % DIVISOR;
+    
+  a = dp[K - 1][N - 2];
 
   // 0번을 안 고른 경우
   for (int start = 1; start < N; ++start)
-    dp[0][0][start] = start;
+    dp[0][start] = start;
 
-  for (int i = 1; i < K; ++i)
+  for (int i = 1; i < K; ++i){
+    dp[i][2 * i] = 0;
     for (int j = 2 * i + 1; j < N; ++j)
-      dp[0][i][j] = (dp[0][i - 1][j - 2] + dp[0][i][j - 1]) % DIVISOR;
+      dp[i][j] = (dp[i - 1][j - 2] + dp[i][j - 1]) % DIVISOR;  
+  }
+    
+  b = dp[K - 1][N - 1];
 
-  cout << (dp[1][K - 1][N - 2] + dp[0][K - 1][N - 1]) % DIVISOR; 
+  cout << (a + b) % DIVISOR; 
 }
 
 void init(){
